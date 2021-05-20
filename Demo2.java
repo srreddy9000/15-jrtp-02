21983a8c292c97f4a6f52ea3c4ba7729f71c17f4
srreddy9000@gmail.com <abc@example.com>
2021-05-20 17:01:30 +0530

    have commit Demo2.java Test.java

--- Demo2.java
+++ Demo2.java
@@ -1,7 +1,7 @@
 public class Test{
 public static void main(String args[]){
 
-int numbers[] = {10,20,30,40,50};
+int numbers[] = {10,20,30,40,50,97};
 
 for(int number : numbers){
 
---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+public static void main(){
+
+System.out.println("this is the test class");
+
+}
+}
